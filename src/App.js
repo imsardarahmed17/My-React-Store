1b441ef2a82e93a6fec9a2e649341c7be9a3125b
Sardar Ahmed <imsardarahmed@gmail.com>
2022-08-09 13:46:03 +0500

    removed old work from app.js and added a counter use react useState hook

--- src/App.js
+++ src/App.js
@@ -1,33 +1,19 @@
 // import logo from './logo.svg';
 import "./App.css";
 import Header from "./Header";
-import Students from "./Students";
-import Child from "./Child";
+// import Students from "./Students";
+// import Child from "./Child";
+import React, { useState } from 'react';
 
 function App() {
-  function getHeader() {
-    return alert("welcome");
-  }
-  function getName(myName) {
-    alert(myName);
-  }
-  return (
-    <div>
-      <Child alert={getName} />
-      <Header myFunc={getHeader} title="Bank of Pakistan" />
-      <Students
-        name="Sardar Ahmed"
-        city="Islamabad"
-        proffesion={"Web developer"}
-      />
-      <Students name="Abrar" city="Shadia" proffesion={"Web developer"} />
-      <Students
-        name="Sajid Iqbal"
-        city="Mianwali"
-        proffesion={"Web developer"}
-      />
-    </div>
-  );
+  const [count, setCount] = useState(0);
+return(
+  <div>
+    <h1>Count is: {count}</h1>
+    <button onClick={() => setCount(count + 1)} >Counter</button>
+    <Header/>
+  </div>
+)  
 }
 
 export default App;
