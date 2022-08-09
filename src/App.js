ccfd460ffb5b9358de30b54fa196d7987415e3a1
Sardar Ahmed <imsardarahmed@gmail.com>
2022-08-09 14:53:27 +0500

    style types practiced

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 // import logo from './logo.svg';
 import "./App.css";
-import Header from "./Header";
+// import Header from "./Header";
 // import Students from "./Students";
 // import Child from "./Child";
-import React, { useState } from 'react';
-
+// import React, { useState } from "react";
+import "./style.css";
+import style from './custom.module.css'
 function App() {
-  const [count, setCount] = useState(0);
-return(
-  <div>
-    <h1>Count is: {count}</h1>
-    <button onClick={() => setCount(count + 1)} >Counter</button>
-    <Header/>
-  </div>
-)  
+  return (
+    <div className="App">
+      <h1><u>Style types</u></h1>
+      <h1 className="primary">Style types: extenal</h1>
+      <h1 style={{ backgroundColor: "skyblue", color: "red", padding: "20px" }}>
+        Style types: internal
+      </h1>
+      <h1 className={style.custom}>Style types: Moduler css </h1>
+    </div>
+  );
 }
 
 export default App;
