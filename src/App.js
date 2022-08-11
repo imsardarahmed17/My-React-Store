19e74bf7f7ebfef91b49bea550b6b01442f0e828
Sardar Ahmed <imsardarahmed@gmail.com>
2022-08-11 12:36:45 +0500

    Added 4 different counters

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
 // import logo from './logo.svg';
 import "./App.css";
-import Header from "./Header";
+// import Header from "./Header";
 // import Students from "./Students";
 // import Child from "./Child";
 import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
 
 function App() {
-  const [status, setStatus] = useState(true);
-return(
-  <div className="App">
-    {
-      status ? <h1>Hello world</h1>: null
-    }
-    <br/>
-    <button onClick={()=> setStatus(!status)}>Toggle</button>
-    
-    <Header/>
-  </div>
-)  
+  const [value, setValue] = useState(0);
+
+  function reset() {
+    return setValue(0);
+  }
+const complexIncrease= ()=>{
+  setTimeout(
+    ()=>{
+      setValue((prevState)=>{
+        return prevState + 1;
+      })
+    } , 2000
+  )
+}
+
+  return (
+    <div className="App">
+      <h1>value is: {value}</h1>
+      <Button
+        className="btn"
+        variant="primary"
+        onClick={() => setValue(value + 1)}
+      >
+        Increase
+      </Button>
+      <Button className="btn" varient="primary" onClick={reset}>
+        Reset
+      </Button>
+      <Button
+        className="btn"
+        varient="primary"
+        onClick={() => setValue(value - 1)}
+      >
+        Decrease
+      </Button>
+      <br />
+      <br />
+      <h1>value is: {value}</h1>
+      <Button className="btn" varient="primary" onClick={complexIncrease}>
+        Comlex counter
+      </Button>
+    </div>
+  );
 }
 
 export default App;
